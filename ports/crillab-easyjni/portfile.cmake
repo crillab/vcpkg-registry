185e9e2ef168565935703fa2797480b3bf313353
tfalque.ext <thibault.falque@exakis-nelite.com>
2023-06-30 19:20:09 +0200

    updates ports\crillab-easyjni to version 0.1.0

---
+++ ports/crillab-easyjni/portfile.cmake
@@ -0,0 +1,47 @@
+# Please see
+# https://vcpkg.readthedocs.io/en/latest/maintainers/vcpkg_from_github/ for
+# details on how to fill out the arguments
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO crillab/easyjni
+REF v0.1.0
+SHA512 f4d3fb7fd3f566243c6a6beecccac1b72c716ba653a1d1aacc4641d13ec25e9c5d919b7a0afa2eaa92ee972172eca76cc35bcb7f5a899bcfda9673fe20a6848c
+    HEAD_REF main
+)
+
+# Set this variable to the name this project installs itself as, i.e. the name
+# that you can use in a find_package(<name> REQUIRED) command call
+set(name crillab-easyjni)
+
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
+    OPTIONS
+    # vcpkg wants CMake package config files in share, so if the project allows
+    # changing the path, then we can do that here
+    #
+    # This option is based on the one provided by cmake-init, for other
+    # projects not following best practices, please refer to their
+    # documentation or their CMake scripts, or in a worst-case scenario, you
+    # have to patch around the project's deficiencies
+    #"-D${name}_INSTALL_CMAKEDIR=lib/share/${name}"
+)
+
+vcpkg_cmake_install()
+
+# If the port's name and the CMake package's name are different, then we can
+# pass the package name here, otherwise no arguments are necessary
+# vcpkg_cmake_config_fixup(PACKAGE_NAME "${name}")
+
+#vcpkg_cmake_config_fixup(CONFIG_PATH cmake)
+
+
+# Remove files that aren't just the build artifacts and empty folders
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
+
+# vcpkg requires a license file to be installed as well
+configure_file(
+    "${SOURCE_PATH}/LICENSE.md"
+    "${CURRENT_PACKAGES_DIR}/share/${PORT}/copyright"
+    COPYONLY
+)
