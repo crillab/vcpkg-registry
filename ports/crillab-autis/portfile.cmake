2b16a2391918cf906fe593f6fb121872be244d19
Romain Wallon <romain.wallon@univ-artois.fr>
2023-07-01 17:52:03 +0200

    :sparkles: Autis port

---
+++ ports/crillab-autis/portfile.cmake
@@ -0,0 +1,47 @@
+# Please see
+# https://vcpkg.readthedocs.io/en/latest/maintainers/vcpkg_from_github/ for
+# details on how to fill out the arguments
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO crillab/autis
+    REF v0.1.0
+    SHA512 faf1e4dbade750de69a7054d3b72b6c391483c9de49cfe921764262e3cedc062d3b5dfbe30d0eac25dd63ac7423b982b3298833406e86ef4bdf877e75990667c
+    HEAD_REF main
+)
+
+# Set this variable to the name this project installs itself as, i.e. the name
+# that you can use in a find_package(<name> REQUIRED) command call
+set(name crillab-autis)
+
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
+    OPTIONS
+    # vcpkg wants CMake package config files in share, so if the project allows
+    # changing the path, then we can do that here
+    #
+    # This option is based on the one provided by cmake-init, for other
+    # projects not following best practices, please refer to their
+    # documentation or their CMake scripts, or in a worst-case scenario, you
+    # have to patch around the project's deficiencies
+    #"-D${name}_INSTALL_CMAKEDIR=lib/share/${name}"
+)
+
+vcpkg_cmake_install()
+
+# If the port's name and the CMake package's name are different, then we can
+# pass the package name here, otherwise no arguments are necessary
+# vcpkg_cmake_config_fixup(PACKAGE_NAME "${name}")
+
+#vcpkg_cmake_config_fixup(CONFIG_PATH cmake)
+
+
+# Remove files that aren't just the build artifacts and empty folders
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
+
+# vcpkg requires a license file to be installed as well
+configure_file(
+    "${SOURCE_PATH}/LICENSE.md"
+    "${CURRENT_PACKAGES_DIR}/share/${PORT}/copyright"
+    COPYONLY
+)
