8f0156e128f4006ba887a806c8a73cad109295d8
Romain Wallon <romain.wallon@univ-artois.fr>
2023-07-20 23:14:09 +0200

    :sparkles: Adds kahypar

---
+++ ports/kahypar/portfile.cmake
@@ -0,0 +1,47 @@
+# Please see
+# https://vcpkg.readthedocs.io/en/latest/maintainers/vcpkg_from_github/ for
+# details on how to fill out the arguments
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO kahypar/kahypar
+    REF 1.3.2
+    SHA512 470476aeb92403f856e6dc01e883e47fff73892e70d0fbc0b8734c27ee0b046cef9b242a9a452fd8e857f3cd338a32d8929d149fb1bbd56a2b14adf2c60b8474
+    HEAD_REF main
+)
+
+# Set this variable to the name this project installs itself as, i.e. the name
+# that you can use in a find_package(<name> REQUIRED) command call
+set(name kahypar)
+
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
+    OPTIONS
+    # vcpkg wants CMake package config files in share, so if the project allows
+    # changing the path, then we can do that here
+    #
+    # This option is based on the one provided by cmake-init, for other
+    # projects not following best practices, please refer to their
+    # documentation or their CMake scripts, or in a worst-case scenario, you
+    # have to patch around the project's deficiencies
+    #"-D${name}_INSTALL_CMAKEDIR=lib/share/${name}"
+)
+
+vcpkg_cmake_install()
+
+# If the port's name and the CMake package's name are different, then we can
+# pass the package name here, otherwise no arguments are necessary
+# vcpkg_cmake_config_fixup(PACKAGE_NAME "${name}")
+
+#vcpkg_cmake_config_fixup(CONFIG_PATH cmake)
+
+
+# Remove files that aren't just the build artifacts and empty folders
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
+
+# vcpkg requires a license file to be installed as well
+configure_file(
+    "${SOURCE_PATH}/LICENSE.txt"
+    "${CURRENT_PACKAGES_DIR}/share/${PORT}/copyright"
+    COPYONLY
+)
