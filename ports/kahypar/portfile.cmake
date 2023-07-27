fb14bd898f2abbf10a95ce20d53da05ad024a642
Romain Wallon <romain.wallon@univ-artois.fr>
2023-07-27 23:06:41 +0200

    :bug: Fixes KaHyPar.

--- ports/kahypar/portfile.cmake
+++ ports/kahypar/portfile.cmake
@@ -32,7 +32,13 @@ vcpkg_execute_required_process(
 )
 
 vcpkg_execute_required_process(
-    COMMAND ${GIT} apply ${CURRENT_PORT_DIR}/CMakeLists.txt.patch ${CURRENT_PORT_DIR}/cmake/variables.cmake.patch ${CURRENT_PORT_DIR}/cmake/docs-ci.cmake.patch ${CURRENT_PORT_DIR}/cmake/project-is-top-level.cmake.patch ${CURRENT_PORT_DIR}/cmake/lint-targets.cmake.patch ${CURRENT_PORT_DIR}/cmake/lint.cmake.patch ${CURRENT_PORT_DIR}/cmake/folders.cmake.patch ${CURRENT_PORT_DIR}/cmake/docs.cmake.patch ${CURRENT_PORT_DIR}/cmake/spell-targets.cmake.patch ${CURRENT_PORT_DIR}/cmake/install-rules.cmake.patch ${CURRENT_PORT_DIR}/cmake/dev-mode.cmake.patch ${CURRENT_PORT_DIR}/cmake/spell.cmake.patch ${CURRENT_PORT_DIR}/cmake/prelude.cmake.patch ${CURRENT_PORT_DIR}/cmake/install-config.cmake.patch ${CURRENT_PORT_DIR}/cmake/coverage.cmake.patch
+    COMMAND ${GIT} clean -fdx
+    WORKING_DIRECTORY ${DOWNLOADS}/kahypar
+    LOGNAME clean
+)
+
+vcpkg_execute_required_process(
+    COMMAND ${GIT} apply ${CURRENT_PORT_DIR}/CMakeLists.txt.patch ${CURRENT_PORT_DIR}/lib/CMakeLists.txt.patch ${CURRENT_PORT_DIR}/cmake/variables.cmake.patch ${CURRENT_PORT_DIR}/cmake/docs-ci.cmake.patch ${CURRENT_PORT_DIR}/cmake/project-is-top-level.cmake.patch ${CURRENT_PORT_DIR}/cmake/lint-targets.cmake.patch ${CURRENT_PORT_DIR}/cmake/lint.cmake.patch ${CURRENT_PORT_DIR}/cmake/folders.cmake.patch ${CURRENT_PORT_DIR}/cmake/docs.cmake.patch ${CURRENT_PORT_DIR}/cmake/spell-targets.cmake.patch ${CURRENT_PORT_DIR}/cmake/install-rules.cmake.patch ${CURRENT_PORT_DIR}/cmake/dev-mode.cmake.patch ${CURRENT_PORT_DIR}/cmake/spell.cmake.patch ${CURRENT_PORT_DIR}/cmake/prelude.cmake.patch ${CURRENT_PORT_DIR}/cmake/install-config.cmake.patch ${CURRENT_PORT_DIR}/cmake/coverage.cmake.patch
     WORKING_DIRECTORY ${DOWNLOADS}/kahypar
     LOGNAME patch
 )
